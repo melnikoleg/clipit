0a55a235b822ad73ce13eeb3e7209d06c3ad8922
Viktor Alm <viktor@almdevelopment.se>
2021-08-21 22:41:53 +0200

    Update clipit.py

--- clipit.py
+++ clipit.py
@@ -1086,7 +1086,7 @@ def process_args(vq_parser, namespace=None):
     }
     aspect_to_size_table = {
         'square': [150, 150],
-        'widescreen': [200, 112]
+        'widescreen': [200, 112],
         'twitter': = [300, 100]
     }
 
