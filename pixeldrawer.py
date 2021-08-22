ec3af15a96aae99a22a29cc1a42fa9d80af16945
Viktor Alm <viktor@almdevelopment.se>
2021-08-22 23:53:43 +0200

    Update pixeldrawer.py

--- pixeldrawer.py
+++ pixeldrawer.py
@@ -17,8 +17,8 @@ from PIL import ImageFile, Image, PngImagePlugin
 pydiffvg.set_print_timing(False)
 
 class PixelDrawer(DrawingInterface):
-    num_rows = 45
-    num_cols = 80
+    num_rows = 100
+    num_cols = 200
     do_mono = False
     pixels = []
 
